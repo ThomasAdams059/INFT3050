d543e1380336b83494c3219c22e10533b21fe4cf
wominicdallace-ui <c3354956@uon.edu.au>
2025-08-21 14:56:50 +1000

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,1 +1,11 @@
+const Header = () => {
+    let dateObj = new Date();
+    return (
+        <>
+            <h1>The Entertainment Guild</h1>
+            <p>Welcome today {dateObj.toLocaleString() + ""} </p>
+        </>
+    );
+}
 
+export default Header;

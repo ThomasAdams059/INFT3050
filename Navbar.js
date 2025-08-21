c460c5d29fb696f04e65f1742b00f4d261561e02
wominicdallace-ui <c3354956@uon.edu.au>
2025-08-21 15:19:05 +1000

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -2,19 +2,19 @@ export default function Navbar() {
     return <nav className="nav">
         <a href="/" className="site-title">The Entertainment Guild</a>
     <ul>
-        <li>
+        <li className="active">
             <a href="/genre">Genre</a>
         </li>
-        <li>
+        <li className="active">
             <a href="/new-releases">New Releases</a>
         </li>
-        <li>
+        <li className="active">
             <a href="/best-sellers">Best Sellers</a>
         </li>
-        <li>
+        <li className="active">
             <a href="/author">Author</a>
         </li>
-        <li>
+        <li className="active">
             <a href="/products">Products</a>
         </li>
     </ul>

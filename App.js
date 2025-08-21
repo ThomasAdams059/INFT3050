2ef311993f6216977370202eee61e23ebb48a87e
wominicdallace-ui <c3354956@uon.edu.au>
2025-08-21 15:10:34 +1000

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import './App.css';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
+import './styles.css';
 
 //The main component
 function App() {

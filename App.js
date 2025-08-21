3fd936710e2269a8b949140fd9b97ed7de572117
wominicdallace-ui <c3354956@uon.edu.au>
2025-08-21 15:37:19 +1000

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
-import Header from './Header';
-import Content from './Content';
-import Footer from './Footer';
+import Navbar from "./Navbar"
+import Home from "./pages/Home"
+import Genre from "./pages/Genre"
+import NewReleases from "./pages/NewReleases"
+import BestSellers from "./pages/BestSellers"
+import Author from "./pages/Author"
+import Products from "./pages/Products"
 import './styles.css';
 
 //The main component
 function App() {
-  return (
-    <div>
-        <Header/>
-        <Content/>
-        <Footer/>
+  let component
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />
+      break
+    case "/genre":
+      component = <Genre />
+      break
+    case "/newReleases":
+      component = <New Releases />
+      break
+    case "/bestSellers":
+      component = <Best Sellers />
+      break
+    case "/author":
+      component = <Author />
+      break
+    case "/products":
+      component = <Products />
+      break
+  }
+  return ( 
+    <>
+      <Navbar />
+    <div className="container">
+      {component}
     </div>
+    </>
   );
 }
 

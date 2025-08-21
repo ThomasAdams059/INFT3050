018a1e3778175a34a614ed956ae744907c50ad23
wominicdallace-ui <c3354956@uon.edu.au>
2025-08-21 15:09:06 +1000

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -1,11 +1,23 @@
-const Header = () => {
-    let dateObj = new Date();
-    return (
-        <>
-            <h1>The Entertainment Guild</h1>
-            <p>Welcome today {dateObj.toLocaleString() + ""} </p>
-        </>
-    );
+export default function Navbar() {
+    return <nav className="nav">
+        <a href="/" className="site-title">The Entertainment Guild</a>
+    <ul>
+        <li>
+            <a href="/genre">Genre</a>
+        </li>
+        <li>
+            <a href="/new-releases">New Releases</a>
+        </li>
+        <li>
+            <a href="/best-sellers">Best Sellers</a>
+        </li>
+        <li>
+            <a href="/author">Author</a>
+        </li>
+        <li>
+            <a href="/products">Products</a>
+        </li>
+    </ul>
+</nav>
 }
-
-export default Header;
+        

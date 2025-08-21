97ad5788f764956893705160a2906f2a15469a63
wominicdallace-ui <c3354956@uon.edu.au>
2025-08-21 15:45:16 +1000

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -1,23 +1,23 @@
 export default function Navbar() {
+    const path = window.location.pathname
     return <nav className="nav">
         <a href="/" className="site-title">The Entertainment Guild</a>
     <ul>
-        <li className="active">
-            <a href="/genre">Genre</a>
-        </li>
-        <li className="active">
-            <a href="/new-releases">New Releases</a>
-        </li>
-        <li className="active">
-            <a href="/best-sellers">Best Sellers</a>
-        </li>
-        <li className="active">
-            <a href="/author">Author</a>
-        </li>
-        <li className="active">
-            <a href="/products">Products</a>
-        </li>
+        <CustomLink href="/genre">Genre</CustomLink>
+        <CustomLink href="/newReleases">New Releases</CustomLink>
+        <CustomLink href="/bestSellers">Best Sellers</CustomLink>
+        <CustomLink href="/author">Author</CustomLink>
+        <CustomLink href="/products">Products</CustomLink>
     </ul>
 </nav>
 }
         
+function CustomLink({ href, children,...props}) {
+    const path = window.location.pathname
+    if 
+    return (
+        <li className={path === href ? "active" : ""}>
+            <a href={href} {...props}>
+                {children}
+            </a>
+        </li>

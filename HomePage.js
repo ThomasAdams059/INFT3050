34170632dd691a4b49bb9457cc68eabed1a1ce62
wominicdallace-ui <c3354956@uon.edu.au>
2025-08-25 15:14:05 +1000

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -1,14 +1,61 @@
-import React from 'react';
+Simport React from 'react';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="mb-8">
       <header>
-        <h1>My Simple React Home Page</h1>
+        <h1 className="text-3xl font-bold text-#495867">Best Sellers</h1>
       </header>
       <main>
         <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
-        <img src="https://via.placeholder.com/300" alt="Placeholder" />
+      </main>
+    </div>
+  <div className="mb-8">
+      <header>
+        <h1 className="text-3xl font-bold text-#495867">New Releases</h1>
+      </header>
+      <main>
+        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
+      </main>
+    </div>
+  <div className="mb-8">
+      <header>
+        <h1 className="text-3xl font-bold text-#495867">Non-Fiction</h1>
+      </header>
+      <main>
+        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
+      </main>
+    </div>
+  <div className="mb-8">
+      <header>
+        <h1 className="text-3xl font-bold text-#495867">Fiction</h1>
+      </header>
+      <main>
+        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
+      </main>
+    </div>
+  <div className="mb-8">
+      <header>
+        <h1 className="text-3xl font-bold text-#495867">Movies</h1>
+      </header>
+      <main>
+        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
+      </main>
+    </div>
+  <div className="mb-8">
+      <header>
+        <h1 className="text-3xl font-bold text-#495867">Games</h1>
+      </header>
+      <main>
+        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
+      </main>
+    </div>
+  <div className="mb-8">
+      <header>
+        <h1 className="text-3xl font-bold text-#495867">Recently Viewed Items</h1>
+      </header>
+      <main>
+        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
       </main>
     </div>
   );

0f58fa86005f2a66f3d6ceea683e1f4fb4a9bf46
ThomasAdams059 <Thomas.Adams05@outlook.com>
2025-10-23 14:44:36 +1100

    Delete PaperTrails_Styles.xaml.cs

--- PaperTrails_Styles.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace PaperTrails_ThomasAdams_c3429938.Resources.Styles;
-
-public partial class PaperTrails_Styles : ResourceDictionary
-{
-	public PaperTrails_Styles()
-	{
-		InitializeComponent();
-	}
-}

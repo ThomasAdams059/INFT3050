e49ed36888219a7e46832a84ffef9b8caeb1465d
wominicdallace-ui <c3354956@uon.edu.au>
2025-10-29 22:10:05 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'; // Import Provider
-import { store } from './store'; // Import your store
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
